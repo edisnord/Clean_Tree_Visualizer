a3a635b8eee77dcae4b72d6b3b335150aed96471
edisnord <edisnord@gmail.com>
2022-12-06 02:23:59 +0100

    slight refactoring

--- treeparser.py
+++ treeparser.py
@@ -3,7 +3,7 @@ import linecache
 
 varname_pattern = r'(^\:\:\s*)(\S+)\s+(\S+)(?=\s*\=)'
 
-class tree:
+class Tree:
     def __init__(self, type: str, var_name: str, raw_declaration: str) -> None:
         self._children = []
         self.type = type
@@ -14,16 +14,16 @@ def findUserSelection(file, type_name, var_name=None):
     with open(file) as source:
         variable_locations = [[x, _treename] for x,varname in enumerate(source, 1) if re.match(varname_pattern, varname, re.IGNORECASE) and (_treename:=(re.search(varname_pattern, varname, re.IGNORECASE).group(2))) == type_name]
         for location in variable_locations:
-            raw_declaration = ""
+            tree_dcl_src = ""
             i = 0
             while True:
                 line = linecache.getline(file, location[0]+i)
                 if i == 0 or ("|" in line and (not ("::" in line) and not ("=" in line))) or line[0] == "\n":
-                    raw_declaration += line
+                    tree_dcl_src += line
                     i += 1
                 else:
                     break
-            print(re.sub("\n|\s{2,}", " ", raw_declaration.strip()))
+            
 
         
         
